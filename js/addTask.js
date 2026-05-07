f2c5e8394efba22b63c9ef849dc10e0eef20d969
Muhammad Naufal Pahlevi <naufal260902@gmail.com>
2026-05-07 16:30:21 +0700

    display tasks | create a user friendly function from taskName and created_at

--- js/addTask.js
+++ js/addTask.js
@@ -10,9 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskData = {
       taskName: document.querySelector("#taskName").value,
       taskPriority: document.querySelector("#taskPriority").value,
+      created_at: nowTime(),
+      isCompleted_at: null,
     };
 
     const result = taskManager.saveData(taskData);
+    formAddTask.reset();
 
     if (result.success) {
       Swal.fire({
@@ -32,4 +35,16 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  function nowTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, 0);
+    const day = String(now.getDate()).padStart(2, 0);
+    const hour = String(now.getHours()).padStart(2, 0);
+    const minute = String(now.getMinutes()).padStart(2, 0);
+    const second = String(now.getSeconds()).padStart(2, 0);
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+  }
 });
